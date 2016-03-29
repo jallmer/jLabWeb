38365939b3f748c7cb66ee76fadbb1e826b96735
Caner Bağcı <canerbagci90@gmail.com>
2016-03-29 15:29:40 +0300

    mysqlqueries.js now works with pooling, which supports multiple queries at the same time and hopefully reconnects after connection is lost

--- mysqlqueries.js
+++ mysqlqueries.js
@@ -1,85 +1,113 @@
 module.exports = {
-    connection: null,
+  connection: null,
+  pool : null,
 
-    connect: function(cbf) {
-        var mysql      = require('mysql');
-        /*
-         * This needs to be replaced with the connection to our mysql database in production.
-         */
-        var os = require("os");
-        var hostname;
-        var user;
-        var password;
-        if(this.startsWith(os.hostname(),"jallmer")) {
-            hostname = "localhost";
-            user = "root";
-            password = "123456";
+connect : function(cbf){
+  var mysql = require('mysql');
+  var os = require("os");
+  var hostname;
+  var user;
+  var password;
+  if (this.startsWith(os.hostname(), "jallmer")) {
+    hostname = "localhost";
+    user = "root";
+    password = "123456";
+  } else {
+    //hostname = "jlab.iyte.edu.tr";
+    hostname = "10.2.24.19";
+    user = "jlabweb";
+    password = "j2016lab";
+  }
+
+  this.pool = mysql.createPool({
+    host: hostname,
+    user: user,
+    password: password,
+    database: "jlabweb"
+  });
+},
+
+
+  getContributors: function(retfunc) {
+    pool.getConnection(function(err, connection) {
+      connection.query("SELECT * FROM contributors", function(err, rows, fields) {
+        if (!err) {
+          retfunc(rows);
         } else {
-            //hostname = "jlab.iyte.edu.tr";
-            hostname = "10.2.24.19";
-            user = "jlabweb";
-            password = "j2016lab";
+          retfunc(null);
         }
-        this.connection = mysql.createConnection({
-          host     : hostname,
-          user     : user,
-          password : password,
-          database : 'jlabweb'
-        });
-        this.connection.connect();
-        cbf(this.connection);
-    },
-
-    disconnect: function() {
-        this.connection.end();
-    },
-
-    getContributors: function(retfunc) {
-        this.connection.query('SELECT * FROM contributors', function(err, rows, fields) {
-            if (!err) {
-                retfunc(rows);
-            } else
-                retfunc(null);
-        });
-    },
-
-    startsWith: function(str, prefix) { // Not the right place for this function ... but for the time being ok.
-        if (str.length < prefix.length)
-            return false;
-        for (var i = prefix.length - 1; (i >= 0) && (str[i] === prefix[i]); --i)
-            continue;
-        return i < 0;
-    },
-
-    logToDB: function(headers,page) {
-        var insert = {};
-        //console.log("Headers: " + JSON.stringify(headers));
-        insert['IP'] = headers['host'];
-        insert['sourcePage'] = headers['referer']+"";
-        insert['targetPage'] = page;
-        insert['browser'] = headers['user-agent'];
-        insert['language'] = headers['accept-language'];
-        this.connection.query("INSERT INTO clickstats SET ?",insert,function(err,result) {
-            if(err)
-                console.log(err);
-        });
-    },
-
-    addPublication: function(bibtexEntry){
-      var insert = {};
-      var fields = ["title", "author", "abstract", "journal", "publisher", "year", "month", "volume", "number", "pages", "keywords", "doi", "url", "pmid", "issn"];
-      for(var key in bibtexEntry.entryTags){
-
-        if(fields.indexOf(key) < 0){
-          continue;
-        }
-        insert[key] = bibtexEntry.entryTags[key];
-      }
-
-      insert['type'] = bibtexEntry.entryType;
-      this.connection.query("INSERT INTO Publications SET ?", insert, function(err, result){
-        if(err)
-          console.log(err);
+        connection.release();
       });
+    });
+
+
+    // this.connection.query('SELECT * FROM contributors', function(err, rows, fields) {
+    //     if (!err) {
+    //         retfunc(rows);
+    //     } else
+    //         retfunc(null);
+    // });
+  },
+
+  startsWith: function(str, prefix) { // Not the right place for this function ... but for the time being ok.
+    if (str.length < prefix.length)
+      return false;
+    for (var i = prefix.length - 1;
+      (i >= 0) && (str[i] === prefix[i]); --i)
+      continue;
+    return i < 0;
+  },
+
+  logToDB: function(headers, page) {
+    var insert = {};
+    //console.log("Headers: " + JSON.stringify(headers));
+    insert['IP'] = headers['host'];
+    insert['sourcePage'] = headers['referer'] + "";
+    insert['targetPage'] = page;
+    insert['browser'] = headers['user-agent'];
+    insert['language'] = headers['accept-language'];
+
+    this.pool.getConnection(function(err, connection) {
+      connection.query("INSERT INTO clickstats SET ?", insert, function(err, rows) {
+        if (err) {
+          console.log(err);
+        }
+        connection.release();
+      });
+    });
+
+    // this.connection.query("INSERT INTO clickstats SET ?",insert,function(err,result) {
+    //     if(err)
+    //         console.log(err);
+    // });
+  },
+
+  addPublication: function(bibtexEntry) {
+    var insert = {};
+    var fields = ["title", "author", "abstract", "journal", "publisher", "year", "month", "volume", "number", "pages", "keywords", "doi", "url", "pmid", "issn"];
+    for (var key in bibtexEntry.entryTags) {
+
+      if (fields.indexOf(key) < 0) {
+        continue;
+      }
+      insert[key] = bibtexEntry.entryTags[key];
     }
+
+    insert['type'] = bibtexEntry.entryType;
+
+    this.pool.getConnection(function(err, connection) {
+      connection.query("INSERT INTO Publications SET ?", insert, function(err, rows) {
+        if (err) {
+          console.log(err);
+        }
+        connection.release();
+      });
+    });
+
+    // this.connection.query("INSERT INTO Publications SET ?", insert, function(err, result){
+    //   if(err)
+    //     console.log(err);
+    // });
+  }
+
 };
